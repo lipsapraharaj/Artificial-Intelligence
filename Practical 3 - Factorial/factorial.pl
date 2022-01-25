d4ab9a127c116965c208b10ca49d964b34450b9c
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2022-01-25 21:44:45 +0530

    Create factorial.pl

---
+++ Practical 3 - Factorial/factorial.pl
@@ -0,0 +1,11 @@
+/* Write a program in PROLOG to implement factorial (N, F) where F represents the
+factorial of a number N.*/
+
+% Factorial of 0 is 1.
+
+factorial(0,1).
+
+
+factorial(N,F):- N >0, 
+  N1 is N-1,
+  factorial(N1,F1), F is N*F1.
