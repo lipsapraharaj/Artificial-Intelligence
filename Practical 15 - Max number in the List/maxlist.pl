c4e51861885819d98ae0061c389f2555b41cf0d5
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2022-02-18 22:10:41 +0530

    Add files via upload

--- Practical 15 - Max number in the List/maxlist.pl
+++ Practical 15 - Max number in the List/maxlist.pl
@@ -1,7 +1,7 @@
-max(X, Y, Z) :- X > Y, Z is X.
-max(X, Y, Z) :- X < Y, Z is Y.
+max(X, Y, Z):- X > Y, Z is X.
+max(X, Y, Z):- X < Y, Z is Y.
 
-maxlist([], 0) :- !.
-maxlist([R], R) :- !.
-maxlist([H|T], R) :- maxlist(T, R1),
+maxlist([], 0):- !.
+maxlist([R], R):- !.
+maxlist([H|T], R):- maxlist(T, R1),
                      max(H, R1, R), !.
