1203000834857f75c3dc7fad237ea657945c94f7
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2022-02-18 21:49:53 +0530

    Add files via upload

--- Practical 11 - Palindrome/palindrome.pl
+++ Practical 11 - Palindrome/palindrome.pl
@@ -1,7 +1,33 @@
-palindrome([]):- write('LIST IS EMPTY.').   % Base case
+/* OBJECTIVE : 
+ 
+To write a prolog program toimplement palindrome(L) which checks whether a 
+list L is a palindrome or not.
 
-palindrome([_]):- write('LIST IS PALINDROME.').  % Base case
+*/
 
-palindrome(L) :- append([H|T], [H], L),          % Recursive case
-                    palindrome(T);
-                    write('LIST IS NOT A PALINDROME!!').
+% CODE:
+
+palindrome([]):- write('LIST IS EMPTY.').  
+
+palindrome([_]):- write('LIST IS PALINDROME.').  
+
+palindrome(L) :- append([H|T], [H], L), 
+                  !,       
+                  palindrome(T);
+                  write('LIST IS NOT A PALINDROME!!').
+
+
+ /**
+ * WORKING:
+ * 
+ *  palindrome([1,2,1]) => append([H|T],[H],[1,2,1])
+      append([1,2],[1],[1,2,1]) => H=1, T=2
+        It checks the first and last element, if they are same,
+         then it removes that from the list and it proceeds with second and 
+          second last element and so on.
+
+          When only one element is left in the list:
+            palindrome([2]) => palindrome([_]) => base case
+*             print  LIST IS PALINDROME.                              
+*                                         
+*/                   
