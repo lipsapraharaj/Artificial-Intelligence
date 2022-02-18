a8294a4d08aa61c2d4109aa40a68d5839b0b5471
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2022-02-18 22:46:47 +0530

    Create merge.pl

---
+++ Practical 18 - Merge the Lists/merge.pl
@@ -0,0 +1,6 @@
+merge(X, [], X).
+merge([], Y, Y).
+
+merge([X|X1], [Y|Y1], [X|Z]):- X < Y, !, merge(X1, [Y|Y1], Z).
+merge([X|X1], [Y|Y1], [X,Y|Z]):- X == Y, !, merge(X1, Y1, Z).
+merge([X|X1], [Y|Y1], [Y|Z]):- X > Y, !, merge([X|X1], Y1, Z).
